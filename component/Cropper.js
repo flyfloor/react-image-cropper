3248d2f1eec90dafe2cc53b71f0c4aef430b1084
jerryshew <jerryshew@gmail.com>
2016-02-29 15:19:21 +0800

    resize and crop

--- component/Cropper.js
+++ component/Cropper.js
@@ -11,7 +11,7 @@ const Cropper = React.createClass({
         };
     },
     getInitialState() {
-        let { originX, originY } = this.props;
+        let { originX, originY, width, rate } = this.props;
         return {
             img_width: '100%',
             img_height: 'auto',
@@ -19,9 +19,13 @@ const Cropper = React.createClass({
             originY,
             startX:0,
             startY: 0,
+            frameWidth: width,
+            frameHeight: width / rate,
             dragging: false,
             maxLeft: 0,
             maxTop: 0,
+            action: null,
+            imgLoaded: false,
         };
     },
 
@@ -31,44 +35,50 @@ const Cropper = React.createClass({
             img_width: container.offsetWidth
         }, () => {
             // calc frame width height
-            let {rate, width, originX, originY} = this.props;
-            const {img_width, img_height} = this.state;
-            const fr_width = width;
-            const fr_height = width / rate;
-            const maxLeft = img_width - width;
-            const maxTop = img_height - fr_height;
+            let {originX, originY} = this.props;
+            const {img_width, img_height, frameWidth, frameHeight} = this.state;
+            const maxLeft = img_width - frameWidth;
+            const maxTop = img_height - frameHeight;
 
-            this.setState({
-                maxLeft, maxTop
-            });
 
-            if (originX + width >= img_width) {
-                originX = img_width - width;
+            if (originX + frameWidth >= img_width) {
+                originX = img_width - frameWidth;
                 this.setState({
                     originX,
                 });
             }
-            if (originY + fr_height >= img_height) {
-                originY = img_height - fr_height;
+            if (originY + frameHeight >= img_height) {
+                originY = img_height - frameHeight;
                 this.setState({
                     originY
                 });
             }
+
+            this.setState({  
+                maxLeft, 
+                maxTop, 
+                imgLoaded: true 
+            });
             // calc clone position
-            this.calcPosition(width, fr_height, originX, originY)
+            this.calcPosition(frameWidth, frameHeight, originX, originY)
+
         });
     },
 
     calcPosition(width, height, left, top){
         const frameNode = ReactDOM.findDOMNode(this.refs.frameNode)
         const cloneImg = ReactDOM.findDOMNode(this.refs.cloneImg)
+        const {img_width, img_height} = this.state;
+
+        if (left < 0) left = 0;
+        if (top < 0) top = 0;
+        if (width + left > img_width) width = img_width;
+        if (height + top > img_height || width < 0 || height < 0) return false;
+
+        if (this.props.onCrop) this.props.onCrop(this.props.src, {left, top, width, height})
        
         frameNode.setAttribute('style', `display:block;left:${left}px;top:${top}px;width:${width}px;height:${height}px`)
         cloneImg.setAttribute('style', `margin-left:${-left}px;margin-top:${-top}px`)
-    },
-
-    computeStyles(left, top){
-
     },
 
     imgOnload(){
@@ -78,89 +88,159 @@ const Cropper = React.createClass({
         }, () => this.initStyles());
     },
 
-    handleDrag(e){
+    createNewFrame(e){
         if (this.state.dragging) {
-            const {originX, originY, startX, startY} = this.state;
-            let _x = e.pageX - startX;
-            let _y = e.pageY - startY;
-            const {width, rate} = this.props;
-            if (e.pageY !== 0 && e.pageX !== 0) {
-                this.inBoundCalc(width, width / rate, _x + originX, _y + originY)
+            let {pageX, pageY} = e;
+            const {rate} = this.props;
+            const {frameWidth, startX, startY, offsetLeft, offsetTop} = this.state;
+
+            let _x = pageX - startX;
+            if (_x > 0) {
+                return this.calcPosition(frameWidth + _x, (frameWidth + _x) / rate, offsetLeft, offsetTop)
             }
+            return this.calcPosition(frameWidth - _x, (frameWidth - _x) / rate,  offsetLeft + _x, offsetTop + _x * rate)
         }
     },
 
-    inBoundCalc(width, height, left, top){
-        let {img_width, img_height, maxLeft, maxTop} = this.state;
-        if (left < 0) left = 0;
-        if (top < 0) top = 0;
-        if (left > maxLeft) left = maxLeft;
-        if (top > maxTop) top = maxTop;
-        this.calcPosition(width, height, left, top)
+    handleDrag(e){
+        if (this.state.dragging) {
+            let {action} = this.state;
+            if (!action) return this.createNewFrame(e)
+            if (action == 'move') return this.frameMove(e)
+            this.frameDotMove(action, e)
+        }
+    },
+
+    frameMove(e){
+        const {originX, originY, startX, startY, frameWidth, frameHeight, maxLeft, maxTop} = this.state;
+        let _x = e.pageX - startX + originX;
+        let _y = e.pageY - startY + originY;
+        if (e.pageX < 0 || e.pageY < 0) return false;
+        if (_x > maxLeft) _x = maxLeft;
+        if (_y > maxTop) _y = maxTop;
+        this.calcPosition(frameWidth, frameHeight, _x, _y)
     },
 
     handleDragStart(e){
+        let action = e.target.getAttribute('data-action');
+        let {pageX, pageY} = e;
         this.setState({
-            startX: e.pageX,
-            startY: e.pageY,
+            startX: pageX,
+            startY: pageY,
             dragging: true,
+            action
         });
+        if (!action) {
+            let container = ReactDOM.findDOMNode(this.refs.container)
+            const {offsetLeft, offsetTop} = container;
+            this.setState({
+                offsetLeft: pageX - offsetLeft,
+                offsetTop: pageY - offsetTop,
+                frameWidth: 2,
+                frameHeight: 2,
+            }, () => {
+                this.calcPosition(2, 2, pageX - offsetLeft, pageY - offsetTop)
+            });
+        }
     },
 
     handleDragStop(e){
         const frameNode = ReactDOM.findDOMNode(this.refs.frameNode)
-        let {offsetLeft, offsetTop} = frameNode;
+        const {offsetLeft, offsetTop, offsetWidth, offsetHeight} = frameNode;
+        const {img_width, img_height} = this.state;
         this.setState({
             originX: offsetLeft,
             originY: offsetTop,
-            dragging: false
+            dragging: false,
+            frameWidth: offsetWidth,
+            frameHeight: offsetHeight,
+            maxLeft: img_width - offsetWidth,
+            maxTop: img_height - offsetHeight,
+            action: null,
         });
     },
 
-    handleDotDrag(dir,e){
-        const {width, rate} = this.props;
+    componentWillUnmount() {
+        document.removeEventListener('mousemove')
+        document.removeEventListener('mousedown')
+        document.removeEventListener('mouseup')
+        document.removeEventListener('mouseleave')
+    },
+
+    frameDotMove(dir,e){
         const {pageX, pageY} = e;
-        const {startX, startY, originX, originY} = this.state;
+        const {rate} = this.props;
+        const {startX, startY, originX, originY, frameWidth, frameHeight} = this.state;
 
         if (pageY !== 0 && pageX !== 0) {
             let _x = pageX - startX;
             let _y = pageY - startY;
+            let new_width = frameWidth + _x;
             switch(dir){
                 case 'ne':
-                    if ( _y == 0 ||  Math.abs(_x / _y) >= rate){
-                        let new_width = width + _x;
-                        return this.inBoundCalc(new_width, new_width / rate, originX, originY - _x / rate )
-                    }
-                    let new_height = width / rate - _y;
-                    return this.inBoundCalc(new_height * rate, new_height, originX, originY + new_height - width / rate)
+                    return this.calcPosition(new_width, new_width/rate, originX, originY - _x / rate)
+                case 'e':
+                    return this.calcPosition(new_width, new_width/rate, originX, originY - _x / rate * .5)
+                case 'se':
+                    return this.calcPosition(new_width, new_width/rate, originX, originY)
+                case 'n':
+                    let new_height = frameHeight - _y;
+                    return this.calcPosition(new_height * rate, new_height, originX + _y * rate * .5, originY + _y)
+                case 'nw':
+                    new_width = frameWidth - _x;
+                    return this.calcPosition(new_width, new_width/rate, originX + _x, originY + _x / rate)
+                case 'w':
+                    new_width = frameWidth - _x;
+                    return this.calcPosition(new_width, new_width / rate, originX + _x, originY + _x / rate * .5)
+                case 'sw':
+                    new_width = frameWidth - _x;
+                    return this.calcPosition(new_width, new_width / rate, originX + _x, originY)
+                case 's':
+                    new_height = frameHeight + _y;
+                    return this.calcPosition(new_height * rate, new_height, originX - _y * rate * .5, originY)
                 default:
                     return
             }
         }
+    },
 
+    crop(){
+        const {frameWidth, frameHeight, originX, originY} = this.state;
+        let canvas = document.createElement('canvas');
+        let img = ReactDOM.findDOMNode(this.refs.img);
+        canvas.width = frameWidth;
+        canvas.height = frameHeight;
+        canvas.getContext("2d").drawImage(img, originX, originY, frameWidth, frameHeight, 0, 0, frameWidth, frameHeight)
+        return canvas.toDataURL();
     },
 
     render() {
+        let className = '_cropper';
+        if (this.state.imgLoaded) className += ' _loaded';
+        if (this.state.dragging) className = `${className} _dragging`;
         return (
-            <div ref="container" className={this.state.dragging ? '_cropper _dragging' : '_cropper'} style={{'position': 'relative', 'height': this.state.img_height}}>
+            <div className={className} onMouseLeave={this.handleDragStop}
+                 ref="container" onMouseMove={this.handleDrag} 
+                 onMouseDown={this.handleDragStart} onMouseUp={this.handleDragStop}
+                style={{'position': 'relative', 'height': this.state.img_height}}>
                 <div className="_source" ref="sourceNode">
-                    <img src={this.props.src} ref='img' onLoad={this.imgOnload} width={this.state.img_width} height={this.state.img_height}/>
+                    <img src={this.props.src} crossOrigin ref='img' onLoad={this.imgOnload} width={this.state.img_width} height={this.state.img_height}/>
                 </div>
                 <div className="_modal"></div>
                 <div className="_frame" ref="frameNode">
                     <div className="_clone">
-                        <img src={this.props.src} ref="cloneImg" width={this.state.img_width}/>
+                        <img src={this.props.src} crossOrigin ref="cloneImg" width={this.state.img_width}/>
                     </div>
-                    <span className="_move" onMouseMove={this.handleDrag} onMouseDown={this.handleDragStart} onMouseUp={this.handleDragStop}></span>
-                    <span className="_dot _dot-ne"></span>
-                    <span className="_dot _dot-n"></span>
-                    <span className="_dot _dot-nw"></span>
-                    <span className="_dot _dot-e"></span>
-                    <span className="_dot _dot-center"></span>
-                    <span className="_dot _dot-w"></span>
-                    <span className="_dot _dot-se"></span>
-                    <span className="_dot _dot-s"></span>
-                    <span className="_dot _dot-sw"></span>
+                    <span className="_move" data-action='move'></span>
+                    <span className="_dot _dot-center" data-action="ne"></span>
+                    <span className="_dot _dot-ne" data-action="ne"></span>
+                    <span className="_dot _dot-n" data-action="n"></span>
+                    <span className="_dot _dot-nw" data-action="nw"></span>
+                    <span className="_dot _dot-e" data-action="e"></span>
+                    <span className="_dot _dot-w" data-action="w"></span>
+                    <span className="_dot _dot-se" data-action="se"></span>
+                    <span className="_dot _dot-s" data-action="s"></span>
+                    <span className="_dot _dot-sw" data-action="sw"></span>
                 </div>
             </div>
         );
--- demo/demo.js
+++ demo/demo.js
@@ -13,14 +13,18 @@ const ImageCropDemo = React.createClass({
         };
     },
 
-    handleCrop(img, state){
+    OnClick(state){
+        let node = this.refs.cropper;
         this.setState({
-            [String(state)]: img
+            [state]: node.crop()
         });
     },
 
-    handleResize(img, coords){
-        console.log(coords)
+    handleMove(coords){
+        const previewNode = ReactDOM.findDOMNode(this.refs.previewNode)
+        const previewHolder = ReactDOM.findDOMNode(this.refs.previewHolder)
+        const {left, top, width, height} = coords;
+        previewNode.setAttribute('style', `margin-left:${-left}px;margin-top:${-top}px;width: ${500 / width}%`)
     },
 
     render() {
@@ -28,28 +32,23 @@ const ImageCropDemo = React.createClass({
             <ul>
                 <li>
                     <h3>Default image crop</h3>
-                    <Cropper src="http://braavos.me/images/posts/college-rock/the-smiths.png"
-                         onCrop={(img) => this.handleCrop(img, 'image')} originX={100} originY={200} onResie={this.handleResize}/>
+                    <Cropper src="http://braavos.me/images/posts/college-rock/the-smiths.png" ref="cropper"/>
+                    <br/>
+                    <button onClick={() => this.OnClick('image')}>crop</button>
                     <h4>after crop</h4>
-                    {this.state.image ? `<img src="${this.state.image}" alt=""/>`: null}
+                    {this.state.image ?  <img width="200" src={this.state.image} alt=""/>: null}
                 </li>
                 {/*<li>
                     <h3>With given origin X and Y</h3>
-                    <Cropper src="http://braavos.me/images/posts/college-rock/the-smiths.png" onCrop={(img) => this.handleCrop(img, 'image1')}  originX={100} originY={100}/>
-                    <h4>after crop</h4>
-                    {this.state.image1 ? `<img src="${this.state.image1}" alt=""/>`: null}
-                </li>
-                /*<li>
-                    <h3>With given origin X and Y</h3>
-                    <Cropper src="http://braavos.me/images/posts/college-rock/the-smiths.png" rate={2} onCrop={(img) => this.handleCrop(img, 'image2')}/>
+                    <Cropper src="http://braavos.me/images/posts/college-rock/the-smiths.png" onCrop={(img, croods) => this.OnClick(img, croods, 'image1')}  originX={100} originY={100}/>
                     <h4>after crop</h4>
                     {this.state.image1 ? `<img src="${this.state.image1}" alt=""/>`: null}
                 </li>
                 <li>
-                    <h3>Allow resize with different size</h3>
-                    <Cropper src="http://braavos.me/images/posts/college-rock/the-smiths.png" onCrop={(img) => this.handleCrop(img, 'image3')} options={options1}/>
+                    <h3>With given rate</h3>
+                    <Cropper src="http://braavos.me/images/posts/college-rock/the-smiths.png" rate={2} onCrop={(img) => this.OnClick(img, 'image2')}/>
                     <h4>after crop</h4>
-                    {this.state.image2 ? `<img src="${this.state.image2}" alt=""/>`: null}
+                    {this.state.image1 ? `<img src="${this.state.image1}" alt=""/>`: null}
                 </li>*/}
             </ul>
         );
